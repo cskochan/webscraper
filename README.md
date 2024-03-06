79de44d1e34cbb3f2c416c89671884c4943a081b
cskochan <cskochan@gmail.com>
2024-03-06 15:18:34 -0800

    Added function for presenting list of book genres in navbar, and returning the url for the page we will want to scrape.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # webscraper
-A web scraper for dewey decimal nerds like me that want to create a catalogue complete with call numbers for their personal library
+A practice web scraper that scrapes books.toscrape.com.
--- webscrape.py
+++ webscrape.py
@@ -1,6 +1,25 @@
-
+from bs4 import BeautifulSoup
+import requests
 
 def main():
+    htmlText = requests.get("http://books.toscrape.com/index.html").text
+    soup = BeautifulSoup(htmlText, 'lxml')
+    genre_url = menuPick(soup)
+    print(genre_url)
+
+def menuPick(soup):
+    navbar = soup.find('ul', class_ = "nav nav-list").li.ul.find_all('li')
+    genre_dict = {'all': "http://books.toscrape.com/index.html"}
+    for genre in navbar:
+        name = genre.text.strip()
+        print(name)
+        genre_dict[name.lower()] = genre.a['href'.strip()]
+    print('Please select a category (or enter "All" for all books).')
+    genrePick = input("Genre: ").lower()
+    if genrePick in genre_dict:
+        return genre_dict[genrePick]
+        
+def write():
     pass
 
 if __name__ == '__main__':
