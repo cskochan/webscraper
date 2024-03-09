64d90fd57f241c02d206a437e14312d484082e80
cskochan <cskochan@gmail.com>
2024-03-08 18:32:46 -0800

    added ability to navigate to various pages, do some scraping there, and write this info to a text document.

--- webscrape.py
+++ webscrape.py
@@ -1,26 +1,69 @@
 from bs4 import BeautifulSoup
 import requests
+import time
+import datetime
 
 def main():
-    htmlText = requests.get("http://books.toscrape.com/index.html").text
+    website = "http://books.toscrape.com"
+    htmlText = requests.get(website).text
     soup = BeautifulSoup(htmlText, 'lxml')
     genre_url = menuPick(soup)
-    print(genre_url)
+    resultsPage(f"{website}/{genre_url}")
+    write("\n*****     *****     *****\n")
 
 def menuPick(soup):
     navbar = soup.find('ul', class_ = "nav nav-list").li.ul.find_all('li')
-    genre_dict = {'all': "http://books.toscrape.com/index.html"}
+    genre_dict = {'all': "index.html"}
     for genre in navbar:
         name = genre.text.strip()
         print(name)
         genre_dict[name.lower()] = genre.a['href'.strip()]
     print('Please select a category (or enter "All" for all books).')
-    genrePick = input("Genre: ").lower()
-    if genrePick in genre_dict:
-        return genre_dict[genrePick]
+    while(True):
+        genrePick = input("Genre: ").lower()
+        if genrePick in genre_dict:
+            write(genrePick.upper())
+            return genre_dict[genrePick]
+        else:
+            print("Invalid Entry. Please try again.")
+
+def resultsPage(link):
+    linkParts = link.split("/")
+    htmlText = requests.get(link).text
+    soup = BeautifulSoup(htmlText, 'lxml')
+    entryCount = soup.find('form', class_ ='form-horizontal').strong.text
+    print(f"{entryCount} entries in total.")
+    books = soup.find_all('article', class_ = 'product_pod')
+    write(str(datetime.datetime.now()))
+    for book in books:
+        title = book.div.img['alt'].strip()
+        info = bookPage(f"{'/'.join(map(str, linkParts[:4]))}/{book.div.a['href'].strip('../')}")
+        write(f"\"{title}\" costs {info[0]} and is {info[1]}")
+        time.sleep(1)
+    try:
+        print(soup.find('li', class_="current").text.strip())
+        next = soup.find('li', class_="next").a['href'].strip()
+        choice = input("See next page? [Y/n]").lower()
+        if choice == 'y' or choice == '':
+            resultsPage(f"{'/'.join(map(str, linkParts[:-1]))}/{next}")
+        else:
+            print("Goodbye")
+    except:
+        pass
+
+def bookPage(link) -> tuple:
+    linkParts = link.split("/")
+    price = availability = "NULL"
+    htmlText = requests.get(link).text
+    soup = BeautifulSoup(htmlText, 'lxml')
+    price = soup.find('p', class_="price_color").text.strip("Â")
+    availability = soup.find('p', class_="instock availability").text.strip()
+    return price, availability
+
         
-def write():
-    pass
+def write(text):
+    with open("results.txt", "a") as file:
+        file.write(f"{text}\n")
 
 if __name__ == '__main__':
     main()
